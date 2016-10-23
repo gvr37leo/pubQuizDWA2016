42f79c479370b211f01405adf0e0c8d54ec6aa6f
Paul Boon <gvr37.p.l.boon@gmail.com>
2016-10-23 18:14:58 +0200

    roomcreat scherm

--- public/quizmaster/quizmaster.js
+++ public/quizmaster/quizmaster.js
@@ -1,12 +1,15 @@
 import React from "react";
-import QuestionApprover from 'questionApprover/questionApprover'
-import QuestionSelector from 'questionSelector/uestionSelector'
-import RoomCreator from 'roomCreator/roomCreator'
-import TeampApprover from 'teamApprover/teamApprover'
+
+import RoomCreator from './roomCreator/roomCreator'
+import TeampApprover from './teamApprover/teamApprover'
+import QuestionSelector from './questionSelector/questionSelector'
+import QuestionApprover from './questionApprover/questionApprover'
 
 export default class Quizmaster extends React.Component{
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
+        this.state= {}  
+        this.state.subView = <RoomCreator />;
         var socket = new WebSocket("ws://localhost:8000");
 
         socket.onopen = function(){
@@ -15,7 +18,20 @@ export default class Quizmaster extends React.Component{
 
         socket.onmessage = function(event){
             var message = event.data
-
+            // switch(message.type){
+            //     case 0:
+            //         this.state.subView = <RoomCreator />
+            //         break;
+            //     case 1:
+            //         this.state.subView = <TeampApprover />
+            //         break;
+            //     case 2:
+            //         this.state.subView = <QuestionSelector />
+            //         break;
+            //     case 3:
+            //         this.state.subView = <QuestionApprover />
+            //         break;
+            // }
         }
 
         socket.onclose = function(){
@@ -29,7 +45,9 @@ export default class Quizmaster extends React.Component{
 
     render(){
         return(
-            <h1>quizmaster</h1>
+            <div>
+                {this.state.subView}
+            </div>
         )
     }
 }
--- public/quizmaster/roomCreator/roomCreator.js
+++ public/quizmaster/roomCreator/roomCreator.js
@@ -3,11 +3,32 @@ import React from 'react';
 export default class RoomCreator extends React.Component{
     constructor(props){
         super(props)
+        this.state = {}
+        this.state.roomPassword = ""
+    }
+
+    goBtnClicked(){
+
+    }
+
+    roomPasswordChange(event){
+        this.setState({roomPassword: event.target.value});
+        // this.state. = event.target.value
     }
 
     render(){
         return(
-            <h1>hello</h1>
+            <div style={{display:'flex', justifyContent:'center', alignItems:'center', height:'100vh'}}>
+                <div className="panel panel-primary">
+                    <div className="panel-heading">Create a room</div>
+                    <div className="panel-body">
+                        <input value={this.state.roomPassword} onChange={this.roomPasswordChange.bind(this)} type="text" className="form-control"/>
+                        <button onClick={this.goBtnClicked.bind(this)} className="btn btn-default">Go!</button>
+                    </div>
+                </div>
+
+            </div>
+
         )
     }
 }
